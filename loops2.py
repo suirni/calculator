266cd9ee52e0bccc3529644e584b3e2035b19dfe
Ziga Mrak <suirni@gmail.com>
2017-10-16 19:29:09 +0200

    initial commit

---
+++ loops2.py
@@ -0,0 +1,10 @@
+# break_loop = False
+#
+# while not break_loop:
+#     x = int(raw_input('Enter the value for x: '))
+#     y = int(raw_input('Enter the value for y: '))
+#
+#     if x % y == 3:
+#         print('ost = 3')
+#     elif x % y == 0:
+#         break_loop = True
